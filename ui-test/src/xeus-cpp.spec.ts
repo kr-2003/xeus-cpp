4e9b7537b633deb25f7e9dc18a341283106ce722
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-02-26 17:35:51 +0530

    added inspect testing

--- ui-test/src/xeus-cpp.spec.ts
+++ ui-test/src/xeus-cpp.spec.ts
@@ -16,8 +16,8 @@ test.beforeAll(async () => {
   await new Promise(resolve => setTimeout(resolve, 10000));
 });
 
-test.describe('xeus-cpp UI Test', () => {
-  test('xeus-cpp should execute some code', async ({ page }) => {
+test.describe('xeus-cpp-lite UI Test', () => {
+  test('xeus-cpp-lite should execute some code', async ({ page }) => {
 
     await page.goto(`${JUPYTERLITE_URL}/lab/index.html`, { timeout: 10000 });
 
@@ -43,6 +43,33 @@ test.describe('xeus-cpp UI Test', () => {
     const screenshot = await outputArea.screenshot();
 
     expect(screenshot).toMatchSnapshot('xeus-cpp-hello-output.png');
+  });
+
+  test.describe('xeus-cpp-lite should inspect documentation', async ({ page }) => {
+    
+    await page.goto(`${JUPYTERLITE_URL}/lab/index.html`, { timeout: 10000 });
+
+    await page.waitForSelector('.jp-LauncherCard', { timeout: 10000 });
+
+    await page.locator('.jp-LauncherCard[title*="C++20"]').first().click();
+
+    await page.waitForSelector('.jp-Notebook', { timeout: 10000 });
+
+    const code = `
+      ?std::vector
+    `;
+    await page.locator('.jp-CodeMirrorEditor').first().click();
+    await page.keyboard.type(code);
+
+    await page.keyboard.press('Control+Enter');
+
+    await page.waitForSelector('.jp-OutputArea-output', { timeout: 20000 });
+    await page.waitForTimeout(2000); // Additional wait for execution
+
+    const outputArea = page.locator('.jp-OutputArea-output').first();
+    const screenshot = await outputArea.screenshot();
+
+    expect(screenshot).toMatchSnapshot('xeus-cpp-inspect-output.png');
   });
 });
 
